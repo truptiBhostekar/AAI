1c962a9aa56eba45eb4cec831c926d6b8de1afdb
truptiBhostekar <73813585+truptiBhostekar@users.noreply.github.com>
2024-03-05 06:20:16 +0530

    new

---
+++ pract7c.py
@@ -0,0 +1,10 @@
+from sklearn import datasets
+from sklearn.cluster import KMeans
+
+iris_df = datasets.load_iris()
+model = KMeans(n_clusters=3, n_init = 10)
+model.fit(iris_df.data)
+predicted_label = model.predict([[7.2, 3.5, 0.8, 1.6]])
+all_predictions = model.predict(iris_df.data)
+print(predicted_label)
+print(all_predictions)
